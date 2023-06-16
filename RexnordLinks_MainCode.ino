a176f5676ee816ea3f31d69165730f308505b06f
TheBetterRegalRexnordGroup <132886176+TheBetterRegalRexnordGroup@users.noreply.github.com>
2023-06-16 13:28:54 +0200

    Add files via upload

--- RexnordLinks_MainCode.ino
+++ RexnordLinks_MainCode.ino
@@ -1,38 +1,39 @@
 #include <HardwareSerial.h>
 
 // variables 
-float DesiredLength = 10.0;         // Depth set by the user on the LCD-screen /// Important: Preset of 40 cm, zo this needs to changed by the user.
-float CurrentLength = 0.0;              // Current length measured by the Optical Encoder in CM
-
-// Optical Encoder
-int OpticalEncoderInternalPullup1 = 2; // Internal Pullup
-int OpticalEncoderInternalPullup2 = 3; // Internal Pullup
-float lengthPerStep = 0.0145; // Length Per Rotation
+bool reverse = false;
+bool insertComplete = false;
+int encoderPin1 = 16;
+int encoderPin2 = 2;
 
 // Piston arduino pins
-int CutterPiston = A1;
-int HolderPiston = A3;
-int AllingmentPiston = A2;
+int CutterPiston1 = 53;
+int CutterPiston2 = A6;
+int HolderPiston = A2;
+int AllingmentPiston = A3;
 
 //Motor arduino pins
+// Twister
 int Motor1 = 13;      ///pwm pin 
 int MotorDir1 = 27;
 int MotorDir2 = 26;
-
+// Twister
 int Motor2 = 12;      // PWM pin
 int MotorDir3 = 28;
 int MotorDir4 = 29;
-
+// Pusher
 int Motor3 = 11;    //pwm pin
 int MotorDir5 = 30;
 int MotorDir6 = 31;
 
-int SlipperySlopePin = 46;
+int Motor4 = 10; // PWM pin
+int MotorDir7 = 32;
+int MotorDir8 = 33;
 
 int StepperDir = 15;
 int stepPin = 14;
 int StepsToNextHole = 58;          //Number of steps so that the next hole is alligned with the inserter.
-int StepsToNextHoleSlow = 20;
+int StepsToNextHoleSlow = 1;
 
 // Start Button
 int startButton1 = 4;
@@ -47,42 +48,39 @@ void setup() {
 
   //set communication speed
   Serial.begin(9600);
-  Serial1.begin(9600);
   
   //initiate motors                                             setting contected pins as output to controll the H-bridge motor controllers
-  pinMode(Motor1,OUTPUT); pinMode(Motor2,OUTPUT); pinMode(Motor3,OUTPUT);
-  pinMode(MotorDir1,OUTPUT); pinMode(MotorDir2,OUTPUT); pinMode(MotorDir3,OUTPUT); pinMode(MotorDir4,OUTPUT); pinMode(MotorDir5,OUTPUT); pinMode(MotorDir6,OUTPUT);
-  pinMode(SlipperySlopePin, OUTPUT);    //output to controll relays for shakermotors on shaketable
+  pinMode(Motor1,OUTPUT); pinMode(Motor2,OUTPUT); pinMode(Motor3,OUTPUT); pinMode(Motor4, OUTPUT);
+  pinMode(MotorDir1,OUTPUT); pinMode(MotorDir2,OUTPUT); pinMode(MotorDir3,OUTPUT); pinMode(MotorDir4,OUTPUT); pinMode(MotorDir5,OUTPUT); pinMode(MotorDir6,OUTPUT); pinMode(MotorDir7,OUTPUT); pinMode(MotorDir8,OUTPUT);
   
   //Start with motors off
-  digitalWrite(MotorDir1,LOW); digitalWrite(MotorDir2,LOW); digitalWrite(MotorDir3,LOW); digitalWrite(MotorDir4,LOW); digitalWrite(MotorDir5,LOW); digitalWrite(MotorDir6,LOW);
-  analogWrite(Motor1,0);  analogWrite(Motor2, 0); analogWrite(Motor3, 0); 
+  digitalWrite(MotorDir1,LOW); digitalWrite(MotorDir2,LOW); digitalWrite(MotorDir3,LOW); digitalWrite(MotorDir4,LOW); digitalWrite(MotorDir5,LOW); digitalWrite(MotorDir6,LOW); digitalWrite(MotorDir7,LOW); digitalWrite(MotorDir8,LOW);
+  analogWrite(Motor1,0);  analogWrite(Motor2, 0); analogWrite(Motor3, 0); analogWrite(Motor4, 0); 
 
   // Initiate Pneumatics
-  pinMode(HolderPiston, OUTPUT); pinMode(AllingmentPiston, OUTPUT); pinMode(CutterPiston, OUTPUT);
-  digitalWrite(HolderPiston, LOW); digitalWrite(AllingmentPiston, LOW); digitalWrite(CutterPiston, LOW);
+  pinMode(HolderPiston, OUTPUT); pinMode(AllingmentPiston, OUTPUT); pinMode(CutterPiston1, OUTPUT); pinMode(CutterPiston2, OUTPUT);
+  digitalWrite(HolderPiston, LOW); digitalWrite(AllingmentPiston, LOW); digitalWrite(CutterPiston1, LOW); digitalWrite(CutterPiston2, LOW);
 
   // Initiate StepperMotor
   pinMode(stepPin, OUTPUT);  pinMode(StepperDir, OUTPUT);
   
   // Initiate Optical Encoder
-  pinMode(2, INPUT_PULLUP); // internal pullup input pin 2 
-  pinMode(3, INPUT_PULLUP); // internal pullup input pin 3
-  attachInterrupt(0, ai0, RISING); // A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
-  attachInterrupt(1, ai1, RISING); // B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
+  pinMode(encoderPin1, INPUT); 
+  pinMode(encoderPin2, INPUT);
   
   // Initiate Start Button
   pinMode(startButton1, OUTPUT);
-  pinMode(startButton2, INPUT_PULLUP);  
+  pinMode(startButton2, INPUT_PULLUP); 
+
+  Serial.println("Setup Done"); 
   
 }
-
 
 /////////////////////////////////////-LOOP-//////////////////////////
 void loop() {
 
   int buttonMemory = digitalRead(startButton2);
-  
+ 
   if (buttonMemory == 0) {
     MainLoop();
   }
@@ -119,29 +117,92 @@ void JoyStick() {
 //////////////////////// Function MainLoop //////////////////////////////////
 
 //// Motor DC /////
+void activateMotorDC(int MotorSpeed, int twisterDirection, int twisterSpeed) {
 
-void activateMotorDC_Fast() {
+  // Activate Motor 1 & 2 - Twist Motor (Clockwise)
+  if (twisterDirection == 0) {
 
-  int MotorSpeed = 255; // Change Motor Speed
+    digitalWrite(MotorDir1,LOW); 
+    digitalWrite(MotorDir2,HIGH); 
 
-  // Activate Motor 1 - Push Motor
-  digitalWrite(MotorDir1,LOW); 
-  digitalWrite(MotorDir2,HIGH); 
+    analogWrite(Motor1, twisterSpeed);
 
-  analogWrite(Motor1, MotorSpeed);
+    digitalWrite(MotorDir3,LOW); 
+    digitalWrite(MotorDir4,HIGH); 
 
-  // Activate Motor 2 - Twist Motor
-  digitalWrite(MotorDir3,LOW); 
-  digitalWrite(MotorDir4,HIGH); 
+    analogWrite(Motor2, twisterSpeed);
+  }
+  
+  // Activate Motor 1 & 2 - Twist Motor (Counter-Clockwise)
+  if (twisterDirection == 1) {
+    
+    digitalWrite(MotorDir1,HIGH); 
+    digitalWrite(MotorDir2,LOW); 
 
-  analogWrite(Motor2, MotorSpeed);
+    analogWrite(Motor1, twisterSpeed);
 
+    digitalWrite(MotorDir3,HIGH); 
+    digitalWrite(MotorDir4,LOW); 
+
+    analogWrite(Motor2, twisterSpeed);
+  }
+  
   // Activate Motor 3 - Push Motor
   digitalWrite(MotorDir5,LOW); 
   digitalWrite(MotorDir6,HIGH); 
 
   analogWrite(Motor3, MotorSpeed);
 
+  // Activate Motor 4 - Push Motor
+  digitalWrite(MotorDir7,HIGH); 
+  digitalWrite(MotorDir8,LOW); 
+
+  analogWrite(Motor4, MotorSpeed);
+  
+}
+
+void activateMotorDCreverse(int MotorSpeed, int twisterDirection, int twisterSpeed) {
+
+  // Activate Motor 1 & 2 - Twist Motor (Clockwise)
+  if (twisterDirection == 0) {
+
+    digitalWrite(MotorDir1,LOW); 
+    digitalWrite(MotorDir2,HIGH); 
+
+    analogWrite(Motor1, twisterSpeed);
+
+    digitalWrite(MotorDir3,LOW); 
+    digitalWrite(MotorDir4,HIGH); 
+
+    analogWrite(Motor2, twisterSpeed);
+  }
+  
+  // Activate Motor 1 & 2 - Twist Motor (Counter-Clockwise)
+  if (twisterDirection == 1) {
+    
+    digitalWrite(MotorDir1,HIGH); 
+    digitalWrite(MotorDir2,LOW); 
+
+    analogWrite(Motor1, twisterSpeed);
+
+    digitalWrite(MotorDir3,HIGH); 
+    digitalWrite(MotorDir4,LOW); 
+
+    analogWrite(Motor2, twisterSpeed);
+  }
+  
+  // Activate Motor 3 - Push Motor
+  digitalWrite(MotorDir5,HIGH); 
+  digitalWrite(MotorDir6,LOW); 
+
+  analogWrite(Motor3, MotorSpeed);
+
+  // Activate Motor 4 - Push Motor
+  digitalWrite(MotorDir7,LOW); 
+  digitalWrite(MotorDir8,HIGH); 
+
+  analogWrite(Motor4, MotorSpeed);
+  
 }
 
 void deactivateMotorDC() {
@@ -178,11 +239,13 @@ void deactivateAlignmentPiston() {
 
 //// Cutting Piston /////
 void activateCuttingPiston() {
-  digitalWrite(CutterPiston, HIGH);
+  digitalWrite(CutterPiston1, HIGH);
+  digitalWrite(CutterPiston2, HIGH);
 }
 
 void deactivateCuttingPiston() {
-  digitalWrite(CutterPiston, LOW);
+  digitalWrite(CutterPiston1, LOW);
+  digitalWrite(CutterPiston2, LOW);
 }
 
 //// Push Down Piston ////
@@ -194,115 +257,110 @@ void deactivatePushDownPiston() {
   digitalWrite(HolderPiston, LOW);
 }
 
-//// Slippery Slope /////
-void activateSlipperySlope() {
-  digitalWrite(SlipperySlopePin, HIGH);
-}
-
-void deactivateSlipperySlope() {
-  digitalWrite(SlipperySlopePin, LOW);
-}
-
-void ai0() {
-  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
-  // Check pin 3 to determine the direction
-  if(digitalRead(3)==LOW) {
-    CurrentLength = CurrentLength + lengthPerStep;
-
-  } else{
-    CurrentLength = CurrentLength - lengthPerStep;
-  }
-}
-   
-void ai1() {
-  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
-  // Check with pin 2 to determine the direction
-  if(digitalRead(2)==LOW) {
-    CurrentLength = CurrentLength - lengthPerStep;
-  } else{
-    CurrentLength = CurrentLength + lengthPerStep;
-  }
-}
-
 ///// Steppen Motor /////
+// Automatic
 void activateStepperMotorForward() {
+
   digitalWrite(StepperDir, HIGH);
-  for (int i = 0; i < StepsToNextHole; i++) {                    //rotate sprokket till next hole is alligned
+
+  for (int i = 0; i < StepsToNextHole / 2; i++) {                    //rotate sprokket till next hole is alligned
     digitalWrite(stepPin, HIGH);
     delayMicroseconds(20000);
     digitalWrite(stepPin, LOW);
     delayMicroseconds(20000);
   }
 
-  delay(1000);
+  delay(5000);
+
+  for (int i = 0; i < StepsToNextHole / 2; i++) {                    //rotate sprokket till next hole is alligned
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(20000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(20000);
+  }
 }
 
-
+// Manual
 void activateStepperMotorSlowForward() {
   digitalWrite(StepperDir, HIGH);
   for (int i = 0; i < StepsToNextHoleSlow; i++) {                    //rotate sprokket till next hole is alligned
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(20000);
+    delayMicroseconds(5000);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(20000);
+    delayMicroseconds(5000);
   }
-
-  delay(1000);
 }
 
 void activateStepperMotorSlowBackward() {
   digitalWrite(StepperDir, LOW);
   for (int i = 0; i < StepsToNextHoleSlow; i++) {                    //rotate sprokket till next hole is alligned
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(20000);
+    delayMicroseconds(5000);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(20000);
+    delayMicroseconds(5000);
   }
-
-  delay(1000);
 }
 
+///// MainLoop /////
 void MainLoop() {
+
+  unsigned long startTime = 0;
+  unsigned long delayTime = 800;
+  int pistonState = 0;
+  int counterPushTwister = 0;
 
   // Reset
   deactivatePushDownPiston();
   deactivateAlignmentPiston();
   deactivateCuttingPiston();
 
-  delay(1000);
-  activateAlignmentPiston();
-  delay(1000);
+  delay(500);
   activatePushDownPiston();
-  delay(1000);
-  activateMotorDC_Fast(); 
-  
-  while (CurrentLength <= DesiredLength) {
-    Serial1.println(CurrentLength);
-    // Wait until Optical Encoder reaches Desired Length
-  } 
-  
-  delay(10000);
+  delay(500);
+  activateAlignmentPiston();
+  delay(500);
+  activateMotorDC(255, 0, 255); 
+
+   while (insertComplete == false) {
+    // Gets Insert Info From Slave
+    reverse = digitalRead(encoderPin1);
+    insertComplete = digitalRead(encoderPin2);
+
+    if (reverse == true) {
+      activateMotorDCreverse(60, 0, 255); 
+    }
+
+    if (millis() - startTime >= delayTime) {
+      startTime = millis();
+      if (pistonState == 0) {
+        deactivatePushDownPiston();
+        delayTime = 100;
+        pistonState = 1;
+      } else {
+        activatePushDownPiston();
+        delayTime = 800;
+        pistonState = 0;
+      }
+    }
+  }
+
+  // Reset inserComplete Variable
+  reverse = false;
+  insertComplete = false;
 
   deactivateMotorDC();
-  delay(3000);
-
+  delay(500);
   activateCuttingPiston();
-  delay(1000);
-
-  // Tot hier gaat het goed
+  delay(500);
   deactivateCuttingPiston();
-  delay(2000);
-  deactivatePushDownPiston();
-  delay(2000);
+  delay(500);
   deactivateAlignmentPiston();
-  delay(2000);
+  delay(500);
+  deactivatePushDownPiston();
+  delay(500);
   activateStepperMotorForward();
-  delay(2000);
+  delay(5000);
 
-  CurrentLength = 0.0;
-
-  Serial.println("Insert Finished");  
+  Serial.println("Finished Pin");  
 
 }
-
-
